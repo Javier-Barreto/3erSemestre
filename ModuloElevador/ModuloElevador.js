e37607ab63f67f14b6580c02d4b1f14b7e27184f
Javier-Barreto <60673812+Javier-Barreto@users.noreply.github.com>
2020-11-03 17:45:45 -0600

    Add files via upload

---
+++ ModuloElevador/ModuloElevador.js
@@ -0,0 +1,123 @@
+var elevador = (function(){
+  //Variable que guarda el piso donde se encuentra, el piso 0 en teoria es planta baja
+  var privatePActual = 0;
+
+  //0 es puerta cerrada y 1 es puerta abierta
+  var privateStateDoor = 0;
+
+  /*function ActualFloor(){
+    return privatePActual;
+  }*/
+
+  function closeDoorTime(){
+    var i =0;
+    do
+    {
+      i++;
+      console.log(i);
+    }while(i<10);
+    privateStateDoor=0;
+    return "Puerta cerrandose";
+  }
+
+  function OpenDoor (){
+    privateStateDoor=1;
+    return "Puerta abierta";
+  }
+
+  function CloseDoor(){
+    privateStateDoor=0;
+    return "Puerta cerrada";
+  }
+
+  function PrivateUpFloor(){
+    if(privatePActual==7)
+    {
+      return "Ya no se puede subir más";
+    }
+    else{
+      if(privateStateDoor==1)
+      {
+        console.log(CloseDoor());
+      }
+
+      console.log(OpenDoor());
+      privatePActual++;
+      console.log("Hemos llegado al piso " + privatePActual);
+      return closeDoorTime();
+    }
+  }
+
+  function PrivateDownFloor(){
+    if(privatePActual==0)
+    {
+      return "Ya no se puede bajar más";
+    }
+    else{
+      if(privateStateDoor==1)
+      {
+        console.log(CloseDoor());
+      }
+
+      console.log(OpenDoor());
+      privatePActual--;
+      console.log("Hemos llegado al piso " + privatePActual);
+      return closeDoorTime();
+    }
+  }
+
+  function GoToFloor(n){
+    if(n>=0&&n<8)
+    {
+      if(privatePActual==n)
+      {
+        return "Usted ya se encuentra en el piso " + privatePActual;
+      }
+      else
+      {
+        if(privatePActual>n)
+        {
+          if(privateStateDoor==1)
+          {
+            console.log(CloseDoor());
+          }
+          do
+          {
+            privatePActual--;
+            console.log(privatePActual);
+          }while(privatePActual!=n);
+        }
+        else
+        {
+          if(privatePActual<n)
+          {
+            if(privateStateDoor==1)
+            {
+              console.log(CloseDoor());
+            }
+            do
+            {
+              privatePActual++;
+              console.log(privatePActual);
+            }while(privatePActual!=n);
+          }
+        }
+        console.log(OpenDoor());
+        console.log("Hemos llegado al piso " + privatePActual + ", ya pueden bajar.");
+        return closeDoorTime();
+      }
+    }
+    else{
+      return "Este elevador nomas tiene 8 pisos y planta baja, ingrese un número entre el 0 y el 7"
+    }
+  }
+
+  return{
+    AbrirPuerta:OpenDoor,
+    CerrarPuerta:CloseDoor,
+    IrAPiso:GoToFloor,
+    Subir1Piso:PrivateUpFloor,
+    Bajar1Piso:PrivateDownFloor/*,
+    PisoActual:ActualFloor*/
+  }
+})();
