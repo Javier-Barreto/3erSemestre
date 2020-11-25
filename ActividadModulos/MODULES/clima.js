991f723945133cad55b9dce3c0c24c4eb6eed74d
Javier-Barreto <60673812+Javier-Barreto@users.noreply.github.com>
2020-11-25 15:21:03 -0600

    Update clima.js

--- ActividadModulos/MODULES/clima.js
+++ ActividadModulos/MODULES/clima.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const statusClima = (city, callback)=>{
-  let urlLatLon = `http://api.weatherstack.com/current?access_key=afcd7b1582bd4c5b0ad8534fd2febb86&query=${city}&format=json`;
+  let urlLatLon = `http://api.weatherstack.com/current?access_key=a34cfae803604bdab8806c48e319f90d&query=${city}&format=json`;
   request(urlLatLon, (error,response,body)=>{
 
   if(error)
